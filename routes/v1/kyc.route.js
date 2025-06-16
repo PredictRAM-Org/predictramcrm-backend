d8e8bc9e0fb4bae1126384a4944cc7146a4dc70e
bhaumikankan <70452964+bhaumikankan@users.noreply.github.com>
2025-06-16 13:15:17 +0530

    Update kyc.route.js
    removed auth and added static auth webhook

--- routes/v1/kyc.route.js
+++ routes/v1/kyc.route.js
@@ -7,7 +7,7 @@ router.post("/esign", auth, KycController.sendESignRequest);
 router.post("/ekyc", auth, KycController.sendEKYCRequest);
 router.get("/ekyc/report", auth, KycController.getEKYCDocReport);
 router.get("/esign/doc", auth, KycController.getESignDoc);
-router.post("/webhook/auth", KycController.kycAuth);
+//router.post("/webhook/auth", KycController.kycAuth);
 router.post("/webhook/send", KycController.kycWebhook);
 
 module.exports = router;
